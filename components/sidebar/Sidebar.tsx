ec819151f44e8df90a02e8292910e2b92b7cc1e0
Jose Ernesto <Joseernestoroldan@gmail.com>
2024-11-16 15:17:45 -0400

    links

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdBackspace } from "react-icons/io";
 import { useState } from "react";
+import Link from "next/link";
 
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
@@ -16,23 +17,39 @@ const Sidebar = () => {
   }
   return (
     <div className="bg-gray-900 bg-opacity-90 top-0 right-0 left-0 h-screen z-20 absolute flex flex-col items-center pt-24 space-y-8">
-      <div className="text-white text-nowrap w-min text-center capitalize text-xl font-semibold">
-        Home
-      </div>
-      <div className="text-white text-nowrap w-min text-center capitalize text-xl font-semibold">
-        About Me
-      </div>
-      <div className="text-white text-nowrap w-min text-center capitalize text-xl font-semibold">
-        Works
-      </div>
-      <div className="text-white text-nowrap w-min text-center capitalize text-xl font-semibold">
-        Contact
-      </div>
-      <div className="text-white text-nowrap w-min text-center capitalize text-xl font-semibold">
-        Learn
-      </div>
+      <Link href={"/"}>
+        <div className="text-white text-nowrap w-min text-center capitalize text-xl font-semibold">
+          Home
+        </div>
+      </Link>
+      <Link href={"/about"}>
+        <div className="text-white text-nowrap w-min text-center capitalize text-xl font-semibold">
+          About Me
+        </div>
+      </Link>
 
-      <IoMdBackspace onClick={() => setOpen(false)} className="text-4xl text-white absolute top-0 right-10" />
+      <Link href={"/workd"}>
+        <div className="text-white text-nowrap w-min text-center capitalize text-xl font-semibold">
+          Works
+        </div>
+      </Link>
+
+      <Link href={"/contact"}>
+        <div className="text-white text-nowrap w-min text-center capitalize text-xl font-semibold">
+          Contact
+        </div>
+      </Link>
+
+      <Link href={"/learn"}>
+        <div className="text-white text-nowrap w-min text-center capitalize text-xl font-semibold">
+          Learn
+        </div>
+      </Link>
+
+      <IoMdBackspace
+        onClick={() => setOpen(false)}
+        className="text-4xl text-white absolute top-0 right-10"
+      />
     </div>
   );
 };
