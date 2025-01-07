06e86083aeb9e01df466c45a75f5f680c4d4b0b4
Jose Ernesto <Joseernestoroldan@gmail.com>
2025-01-07 11:44:02 -0400

    Update: Contact Button

--- app/(sections)/contact/ContactButton.tsx
+++ app/(sections)/contact/ContactButton.tsx
@@ -7,7 +7,7 @@ const ContactButton = () => {
   return (
     <button
       aria-disabled={pending}
-      className="w-1/2 bg-gray-300 dark:bg-gray-700 rounded-xl p-4 border border-gray-500 hover:bg-gray-400"
+      className="w-full max-w-xl bg-gray-300 dark:bg-gray-700 rounded-xl p-4 border border-gray-500 hover:bg-gray-400"
     >
       Send
     </button>
