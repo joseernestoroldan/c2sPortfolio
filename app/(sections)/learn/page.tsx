5f28b284c9dc18f60944d0427f57af583bfa65c1
Jose Ernesto <Joseernestoroldan@gmail.com>
2025-01-07 11:48:07 -0400

    Update: Learn Page Message

--- app/(sections)/learn/page.tsx
+++ app/(sections)/learn/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LearnPage = () => {
   return (
-    <div className='w-full h-auto pt-44 max-w-5xl mx-auto flex items-center flex-col'>LearnPage</div>
+    <div className='w-full h-auto pt-44 max-w-5xl mx-auto flex items-center flex-col text-5xl text-gray-500'>Coming soon ...</div>
   )
 }
 
