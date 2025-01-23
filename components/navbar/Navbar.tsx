9ce2267d87302be0149ec24e9b00c1fdf1727895
Jose Ernesto <Joseernestoroldan@gmail.com>
2025-01-22 23:59:19 -0400

    p navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div className="w-full bg-[#D6DBDC] dark:bg-gray-900 fixed top-0 left-1/2 -translate-x-1/2 z-30">
       <div className="w-full max-w-4xl mx-auto bg-[#D6DBDC] dark:bg-gray-900">
-        <div className="w-full bg-[#D6DBDC] dark:bg-gray-900">
+        <div className="w-full bg-[#D6DBDC] dark:bg-gray-900 pr-4">
           <Social />
         </div>
         <div className="w-full bg-[#D6DBDC] dark:bg-gray-900 py-4  flex flex-row justify-between items-center">
@@ -22,7 +22,7 @@ const Navbar = () => {
           <div className="hidden md:inline-block">
             <Menu />
           </div>
-          <div className="inline-block md:hidden">
+          <div className="inline-block md:hidden pr-4">
             <Sidebar />
           </div>
         </div>
