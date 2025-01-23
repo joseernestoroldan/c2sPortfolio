f30eb61c16f5eacbf7c2425443493b1e807f5176
Jose Ernesto <Joseernestoroldan@gmail.com>
2025-01-23 00:56:43 -0400

    social links

--- components/social/Social.tsx
+++ components/social/Social.tsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import { FaWhatsapp } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-
+import { FaWhatsapp as WhatsAppIcon } from "react-icons/fa";
+import { FaInstagram as IgIcon } from "react-icons/fa";
+import { FaYoutube as YoutubeIcon } from "react-icons/fa";
+import { FaGithub as GithubIcon } from "react-icons/fa";
+import Link from "next/link";
 
 const Social = () => {
   return (
-    <div className='w-full flex flex-row justify-end items-center space-x-4 py-4'>
-        <FaWhatsapp className='text-xl opacity-75'/>
-        <FaInstagram className='text-xl opacity-75'/>
-        <FaYoutube className='text-xl opacity-75'/>
-        <FaGithub className='text-xl opacity-75'/>
-        
-    </div>
-  )
-}
+    <div className="w-full flex flex-row justify-end items-center space-x-4 py-4">
+      <Link href="https://wa.me/584245042502" target="_blank">
+        <WhatsAppIcon className="text-xl opacity-75" />
+      </Link>
 
-export default Social
+      <Link href={"https://www.instagram.com/code2steps/"} target="_blank">
+        <IgIcon className="text-xl opacity-75" />{" "}
+      </Link>
+      <YoutubeIcon className="text-xl opacity-75" />
+      <Link href={"https://github.com/joseernestoroldan"} target="_blank">
+        <GithubIcon className="text-xl opacity-75" />
+      </Link>
+    </div>
+  );
+};
+
+export default Social;
